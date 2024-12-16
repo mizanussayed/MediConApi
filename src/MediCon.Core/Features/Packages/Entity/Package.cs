5232350454ce90b24f0b63c8a52d848d01e3d5ed
Mizanus Sayed <mizandusp@gmail.com>
2024-12-17 02:55:48 +0600

    add more entity

---
+++ src/MediCon.Core/Features/Packages/Entity/Package.cs
@@ -0,0 +1,13 @@
+﻿using MediCon.Core.Configurations.CommonModel;
+
+
+namespace MediCon.Core.Features.Packages.Entity;
+
+public sealed class Package : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string IconUrl { get; set; } = string.Empty;
+    public decimal StartFromPrice { get; set; }
+    public string ShortDescription { get; set; } = string.Empty;
+
+}
